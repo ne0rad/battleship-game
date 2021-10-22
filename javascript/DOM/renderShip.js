6a32becb0ff29936f097df4656e5707f01b72c66
Radminas Usackas <usackasr@gmail.com>
2021-10-22 21:53:07 +0100

    Updated style

--- javascript/DOM/renderShip.js
+++ javascript/DOM/renderShip.js
@@ -1,16 +1,16 @@
 const renderShip = {
     setShip(playerID, x, y) {
-        let square = document.getElementById(`${playerID}_${x}${y}`);
+        let square = document.getElementById(`${playerID}_${x}.${y}`);
         square.classList.add('ship');
     },
 
     setHit(playerID, x, y) {
-        let square = document.getElementById(`${playerID}_${x}${y}`);
+        let square = document.getElementById(`${playerID}_${x}.${y}`);
         square.classList.add('hit');
     },
 
     setMiss(playerID, x, y) {
-        let square = document.getElementById(`${playerID}_${x}${y}`);
+        let square = document.getElementById(`${playerID}_${x}.${y}`);
         square.classList.add('miss');
     }
 }
--- javascript/game.js
+++ javascript/game.js
@@ -1,6 +1,24 @@
 import { renderMenu } from "./DOM/renderMenu.js";
 import { renderBoard } from './DOM/renderBoard.js';
+import { renderShip } from './DOM/renderShip.js';
+
 
 renderMenu();
 renderBoard(1); // playerID = 1
 renderBoard(2); // playerID = 2
+
+renderShip.setShip(1, 2, 5);
+renderShip.setShip(1, 2, 6);
+renderShip.setShip(1, 2, 7);
+renderShip.setShip(1, 2, 8);
+
+
+renderShip.setHit(1, 2, 1);
+renderShip.setHit(1, 3, 1);
+renderShip.setHit(1, 4, 1);
+
+renderShip.setMiss(1, 7, 3);
+renderShip.setMiss(1, 7, 4);
+renderShip.setMiss(1, 7, 5);
+renderShip.setMiss(1, 7, 6);
+renderShip.setMiss(1, 6, 6);

bbf845275923bda7f7043f659191f001a3b641bf
ne0rad <usackasr@gmail.com>
2021-10-30 18:51:26 +0100

    Added cpuMove.js function

--- javascript/modules/cpuMove.js
+++ javascript/modules/cpuMove.js
@@ -1,5 +1,14 @@
 function cpuMove(availableSquares) {
-    
+    const availableLoc = [];
+    availableSquares.forEach(loc => {
+        let tempLoc = loc.split('.');
+        availableLoc.push([tempLoc[0], tempLoc[1]]);
+    });
+    let rand = Math.floor(Math.random() * availableLoc.length);
+    return {
+        x: availableLoc[rand][0],
+        y: availableLoc[rand][1]
+    }
 }
 
 export { cpuMove }

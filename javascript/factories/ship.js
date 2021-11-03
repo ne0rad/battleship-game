96b5ff051c6b364e3c663ce7a2872f8d14bff8fc
Radminas Usackas <usackasr@gmail.com>
2021-11-03 17:39:32 +0000

    Updated ship.js

--- javascript/factories/ship.js
+++ javascript/factories/ship.js
@@ -2,6 +2,7 @@ import { renderShip } from "../DOM/renderShip.js";
 
 function ship(playerID, x, y, length, isVertical) {
     const loc = [];
+    const surroundingLoc = [];
     let health = length;
     placeShip();
 
@@ -10,10 +11,34 @@ function ship(playerID, x, y, length, isVertical) {
             if (!isVertical) {
                 renderShip.setShip(playerID, x + i, y);
                 loc.push([x + i, y]);
+                if (i === 0) {
+                    surroundingLoc.push([x - 1, y]);
+                    surroundingLoc.push([x - 1, y + 1]);
+                    surroundingLoc.push([x - 1, y - 1]);
+                }
+                surroundingLoc.push([x + i, y + 1]);
+                surroundingLoc.push([x + i, y - 1]);
+                if (i === length - 1) {
+                    surroundingLoc.push([x + length, y]);
+                    surroundingLoc.push([x + length, y - 1]);
+                    surroundingLoc.push([x + length, y + 1]);
+                }
             }
             else {
                 renderShip.setShip(playerID, x, y + i);
                 loc.push([x, y + i]);
+                if (i === 0) {
+                    surroundingLoc.push([x, y - 1]);
+                    surroundingLoc.push([x + 1, y - 1]);
+                    surroundingLoc.push([x - 1, y - 1]);
+                }
+                surroundingLoc.push([x + 1, y + i]);
+                surroundingLoc.push([x - 1, y + i]);
+                if (i === length - 1) {
+                    surroundingLoc.push([x, y + length]);
+                    surroundingLoc.push([x - 1, y + length]);
+                    surroundingLoc.push([x + 1, y + length]);
+                }
             }
         }
     }
@@ -35,7 +60,11 @@ function ship(playerID, x, y, length, isVertical) {
         });
     }
 
-    return { hit }
+    function getSurroundingLoc() {
+        return surroundingLoc;
+    }
+
+    return { hit, getSurroundingLoc }
 }
 
 export { ship }
